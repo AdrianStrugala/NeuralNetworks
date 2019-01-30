fa9035ff0a85122ea3d20473f0c29c8434c53223
Adrian Strugala <adrian.strugala@comparex.com>
2019-01-30 12:33:11 +0100

    added condition where to stop error

--- C#/AI/NeuralNetworks/SingleLayer.cs
+++ C#/AI/NeuralNetworks/SingleLayer.cs
@@ -2,7 +2,6 @@
 {
     using ConsoleTableExt;
     using System;
-    using System.Collections.Generic;
     using System.Data;
     using System.Linq;
 
@@ -19,11 +18,11 @@
 
         private static readonly double[][] X =
         {
-                      //husk //no of legs
-            new double[] {1, 0, (double) Environment.Water}, //fish
-            new double[] {0, 2, (double) Environment.Air}, //bird
-            new double[] {0, 4, (double) Environment.Ground}, //alpaca
-            new double[] {0, 2, (double)Environment.Ground }  //human
+                      //husk //no of legs                   //hoof
+            new double[] {1, 0, (double) Environment.Water, 0}, //fish
+            new double[] {0, 2, (double) Environment.Air, 0}, //bird
+            new double[] {0, 4, (double) Environment.Ground, 1}, //alpaca
+            new double[] {0, 2, (double)Environment.Ground, 0 }  //human
         };
 
         private static readonly double[][] Y =  {
@@ -35,16 +34,18 @@
 
         //Parameters
         public static double Beta = 1;       //output function factor
-        public static double Eta = 0.1;      //learning factor
+        public static double Eta = 1;      //learning factor
         public static int N;      //no of Neurons in Input Layer
         public static int M;     //no of Neurons in Output Layer
+
+        public const double AcceptedError = 0.01;
 
         public static readonly int Epochs = 100000;  //no of iterations of learing phase
 
 
         static double[][] Weights;
 
-        private static readonly List<double> ErrorList = new List<double>();
+        private static double[] ErrorList;
 
         private static readonly Random Random = new Random();
 
@@ -53,6 +54,11 @@
             N = X[0].Length;
             M = Y.Length;
 
+            ErrorList = new double[M];
+            for (int i = 0; i < ErrorList.Length; i++)
+            {
+                ErrorList[i] = Double.MaxValue;
+            }
 
             // INITIALIZATION OF WEIGHT MATRIX
             Weights = Matrix.Create(N, M);
@@ -68,27 +74,27 @@
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BEFORE TRAINING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Check();
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TRAINING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Train();
+            var noOfEpochsToLearn = Train();
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AFTER TRAINING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Check();
 
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EPOCHS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine(noOfEpochsToLearn);
 
-            //            foreach (var error in ErrorList)
-            //            {
-            //                Console.Write(error + " ");
-            //            }
 
             Console.ReadKey();
         }
 
-        static void Train()
+        static int Train()
         {
+            bool finishLearning = false;
+
+            int e = 0;
             //LEARNING
-            for (int e = 0; e < Epochs; e++)
+            for (; e < Epochs && !finishLearning; e++)
             {
                 // CHOOSING RANDOM EXAMPLE
-                int l = Random.Next(X.Length);
+                int l = e % X.Length;
 
                 double[][] input = Matrix.Create(1, N);
                 input[0] = X[l];
@@ -110,17 +116,20 @@
                 Weights = Matrix.Add(Weights, diffForWeights);
 
 
-                //STORE ERROR Błąd średnikwadratowy
-                double error = 0;
-                for (int i = 0; i < diff.Length; i++)
+                //STORE Medium Square Error
+                double error = MediumSquareError(diff);
+                ErrorList[l] = error;
+
+                foreach (var err in ErrorList)
                 {
-                    error = diff[i] * diff[i];
-                }
-
-                error = Math.Sqrt(error);
-
-                ErrorList.Add(error);
+                    if (err > AcceptedError)
+                    {
+                        break;
+                    }
+                    finishLearning = true;
+                }             
             }
+            return e;
         }
 
 
@@ -128,13 +137,15 @@
         {
             //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CHECK~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
             DataTable dt = new DataTable();
-            dt.Columns.Add("x1");
-            dt.Columns.Add("x2");
-            dt.Columns.Add("x3");
-            dt.Columns.Add("y1");
-            dt.Columns.Add("y2");
-            dt.Columns.Add("y3");
-            dt.Columns.Add("y4");
+            dt.Columns.Add("husk");
+            dt.Columns.Add("no of legs");
+            dt.Columns.Add("enviroment");
+            dt.Columns.Add("hoof");
+
+            dt.Columns.Add("fish");
+            dt.Columns.Add("bird");
+            dt.Columns.Add("alpaca");
+            dt.Columns.Add("human");
 
             for (int l = 0; l < X.Length; l++)
             {
@@ -149,11 +160,12 @@
                 row[0] = x[0][0];
                 row[1] = x[0][1];
                 row[2] = x[0][2];
+                row[3] = x[0][3];
 
-                row[3] = y[0];
-                row[4] = y[1];
-                row[5] = y[2];
-                row[6] = y[3];
+                row[4] = y[0];
+                row[5] = y[1];
+                row[6] = y[2];
+                row[7] = y[3];
                 dt.Rows.Add(row);
             }
 
@@ -181,6 +193,17 @@
             }
 
             return y;
+        }
+
+        public static double MediumSquareError(double[] vector)
+        {
+            double error = 0;
+            foreach (var t in vector)
+            {
+                error += t * t;
+            }
+            error = Math.Sqrt(error);
+            return error;
         }
     }
 }
